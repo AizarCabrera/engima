7a631fcba6383eb2420b508987e844a9e5d0c27e
Aizar <aizar.cabrera@icloud.com>
2018-11-02 17:22:47 -0600

    adds test_it_can_get_last_four_digits_of_square_date passing

--- lib/offsetgen.rb
+++ lib/offsetgen.rb
@@ -2,10 +2,24 @@ require './helper'
 
 class OffsetGen
 
-  def get_today_date
-    Time.now.strftime("%d/%m/%Y")
+  attr_reader :offsets
+
+  def initialize
+    @offsets = last_four_digits
   end
 
+  def today_date
+    Time.new
+  end
 
+  def square_date
+    (today_date.day.to_s + today_date.month.to_s + (today_date.year % 100).to_s).to_i ** 2
+  end
+
+  def last_four_digits
+    square_date_string  = square_date.to_s
+    last_four = square_date_string[-4..-1]
+    last_four.chars.map(&:to_i)
+  end
 
 end
--- test/offsetgen_test.rb
+++ test/offsetgen_test.rb
@@ -11,7 +11,7 @@ class OffsetGenTest < Minitest::Test
   def test_it_can_get_todays_date
     offset_gen = OffsetGen.new
 
-    assert_instance_of String, offset_gen.get_today_date
+    assert_instance_of Time, offset_gen.today_date
   end
 
   def test_it_can_square_todays_date
@@ -20,5 +20,11 @@ class OffsetGenTest < Minitest::Test
     assert_instance_of Integer, offset_gen.square_date
   end
 
+  def test_it_can_get_last_four_digits_of_square_date
+    offset_gen = OffsetGen.new
+
+    assert_instance_of Array, offset_gen.last_four_digits
+    assert_instance_of Integer, offset_gen.last_four_digits.first
+  end
 
 end
