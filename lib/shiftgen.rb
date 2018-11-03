9d13873ea9aed70e86f489b3058d92a046a44b7e
Aizar <aizar.cabrera@icloud.com>
2018-11-02 19:04:02 -0600

    adds test_it_has_correct_attributes passing

--- lib/shiftgen.rb
+++ lib/shiftgen.rb
@@ -1,4 +1,12 @@
 require './helper'
 
 class ShiftGen
+
+  attr_reader :key, :offset
+
+  def initialize(key = KeyGen.new.key_maker, offset = OffsetGen.new.offsets)
+    @key    = key
+    @offset = offset
+  end
+
 end
--- test/shiftgen_test.rb
+++ test/shiftgen_test.rb
@@ -4,11 +4,16 @@ class ShiftGenTest < Minitest::Test
 
   def test_it_exists
     shiftgen= ShiftGen.new
+
     assert_instance_of ShiftGen, shiftgen
   end
 
 
+  def test_it_has_correct_attributes
+    shiftgen= ShiftGen.new
 
-
+    assert_instance_of Array, shiftgen.key
+    assert_instance_of Array, shiftgen.offset
+  end
 
 end
