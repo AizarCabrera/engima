5377fa36bbfe90a4bc83f46ba2e6f118956c5d48
Aizar <aizar.cabrera@icloud.com>
2018-11-03 11:17:11 -0600

    changes alphabet position

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -6,33 +6,7 @@ class Enigma
 
   def initialize(shift = ShiftGen.new)
     @shift    = shift
-    @alphabet = ["a",
-                 "b",
-                 "c",
-                 "d",
-                 "e",
-                 "f",
-                 "g",
-                 "h",
-                 "i",
-                 "j",
-                 "k",
-                 "l",
-                 "m",
-                 "n",
-                 "o",
-                 "p",
-                 "q",
-                 "r",
-                 "s",
-                 "t",
-                 "u",
-                 "v",
-                 "w",
-                 "x",
-                 "y",
-                 "z",
-                 " "]
+    @alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
   end
 
   def get_new(position)
