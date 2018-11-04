68da6b7b1f5d05a90dbaecab8d009ba65114a9b5
Aizar <aizar.cabrera@icloud.com>
2018-11-04 15:39:10 -0700

    adds test_it_can_generate_a_random_key_as_array, repaired and passing

--- test/keygen_test.rb
+++ test/keygen_test.rb
@@ -10,15 +10,15 @@ class KeyGenTest < Minitest:: Test
     assert_instance_of KeyGen, @key_gen
   end
 
-  def test_it_can_generate_random_number_for_key
+  def test_it_can_generate_random_number_for_key_if_not_key_is_provided
     assert_instance_of Array, @key_gen.format_key('')
     refute_equal 99999, @key_gen.format_key('').join.to_i
   end
 
-  # def test_it_can_generate_a_random_key_as_array
-  #   assert_equal 4,@key_gen.key_maker.length
-  #   assert_instance_of Array,@key_gen.key_maker
-  #   assert_instance_of Integer,@key_gen.key_maker.first
-  # end
+  def test_it_can_generate_a_random_key_as_array
+    assert_equal 4,@key_gen.key_maker.length
+    assert_instance_of Array,@key_gen.key_maker
+    assert_instance_of Integer,@key_gen.key_maker.first
+  end
 
 end
