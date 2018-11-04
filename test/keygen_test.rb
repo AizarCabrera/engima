9c35dc688b05f6b3ba9c08d1ec4a63b1a5c649dc
Aizar <aizar.cabrera@icloud.com>
2018-11-04 15:37:23 -0700

    adds test_it_can_generate_random_number_for_key passing, changed the name of method and adjusted it to take aa default value or create a random key

--- test/keygen_test.rb
+++ test/keygen_test.rb
@@ -3,18 +3,18 @@ require './helper'
 class KeyGenTest < Minitest:: Test
 
   def setup
-    @key_gen = KeyGen.new('12345')
+    @key_gen = KeyGen.new('')
   end
 
   def test_it_exists
     assert_instance_of KeyGen, @key_gen
   end
-  #
-  # def test_it_can_generate_random_number_for_key
-  #   assert_instance_of Array, @key_gen.random
-  #   refute_equal 99999, @key_gen.random.join.to_i
-  # end
-  #
+
+  def test_it_can_generate_random_number_for_key
+    assert_instance_of Array, @key_gen.format_key('')
+    refute_equal 99999, @key_gen.format_key('').join.to_i
+  end
+
   # def test_it_can_generate_a_random_key_as_array
   #   assert_equal 4,@key_gen.key_maker.length
   #   assert_instance_of Array,@key_gen.key_maker
