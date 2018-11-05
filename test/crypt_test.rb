6bf4000db11c3ee6f38e7a5b9f9d79676ab9e83c
Aizar <aizar.cabrera@icloud.com>
2018-11-04 19:08:32 -0700

    adds tests it exists for crypt test

--- test/crypt_test.rb
+++ test/crypt_test.rb
@@ -1,5 +1,13 @@
 require './helper'
 
 class CryptTest < Minitest::Test
-  
+
+  def setup
+    @c= Crypt.new('','','')
+  end
+
+  def test_it_exists
+    assert_instance_of Crypt, @c
+  end
+
 end
