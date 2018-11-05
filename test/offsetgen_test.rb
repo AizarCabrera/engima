82fc88ebc6662ff65569acc4ab0b25b4e7cb44a7
Aizar <aizar.cabrera@icloud.com>
2018-11-04 17:44:00 -0700

    adds test_it_can_format_offset_even_when_offset_is_not_provided passing

--- test/offsetgen_test.rb
+++ test/offsetgen_test.rb
@@ -25,8 +25,16 @@ class OffsetGenTest < Minitest::Test
   end
 
   def test_it_can_get_last_four_digits_of_square_date_if_no_offset_is_provided
-    assert_instance_of Array, @offset_gen.set_offset((Time.new))
-    assert_instance_of Integer, @offset_gen.set_offset((Time.new)).first
+    assert_instance_of Array, @offset_gen.set_offset(Time.new)
+    assert_instance_of Integer, @offset_gen.set_offset(Time.new).first
+  end
+
+  def test_it_can_format_offset_even_when_offset_is_not_provided
+    assert_equal [9, 9, 2, 4], @offset_gen.format_offset('041118')
+    assert_instance_of Array, @offset_gen.format_offset('041118')
+    assert_instance_of Array, @offset_gen.format_offset('')
+    assert_instance_of Integer, @offset_gen.format_offset('041118').first
+    assert_instance_of Integer, @offset_gen.format_offset('').first
   end
 
 end
