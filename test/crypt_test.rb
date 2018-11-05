0e91575a2d15de84edb0eae6a6d03165e2064eb6
Aizar <aizar.cabrera@icloud.com>
2018-11-04 19:13:20 -0700

    adds test_it_has_attributes passing

--- test/crypt_test.rb
+++ test/crypt_test.rb
@@ -10,4 +10,13 @@ class CryptTest < Minitest::Test
     assert_instance_of Crypt, @c
   end
 
+  def test_it_has_attributes
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    assert_equal expected, @c.alphabet
+    assert_equal '', @c.message
+    assert_instance_of KeyGen, @c.key
+    assert_instance_of OffsetGen, @c.offset
+    assert_instance_of ShiftGen, @c.shift
+  end
+
 end
