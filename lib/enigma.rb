0c21e794c7089c4a5d347604abf0661482fc7d41
Aizar <aizar.cabrera@icloud.com>
2018-11-05 12:11:09 -0700

    removes unnecesary alphabet from enigma

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,10 +4,6 @@ class Enigma
 
   attr_reader :alphabet
 
-  def initialize
-    @alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
-  end
-
   def encrypt(message, key = '', offset = '')
     c = Crypt.new(message, key, offset)
     c.encrypt
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -10,11 +10,6 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @e
   end
 
-  def test_it_has_an_alphabet
-    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
-    assert_equal expected, @e.alphabet
-  end
-
   def test_it_can_encrypt_one_character
     assert_equal ({:encryption=>"l", :key=>"12345", :offset=>"9924"}), @e.encrypt('r',"12345","041118")
   end
