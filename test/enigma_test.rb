a5e695cd7287d7f45bd08a2df31d9a1e51fcd59c
Aizar <aizar.cabrera@icloud.com>
2018-11-05 12:12:18 -0700

    adds a better description for tests on enigma class

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -10,19 +10,19 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @e
   end
 
-  def test_it_can_encrypt_one_character
+  def test_it_can_encrypt_one_character_and_returns_a_hash
     assert_equal ({:encryption=>"l", :key=>"12345", :offset=>"9924"}), @e.encrypt('r',"12345","041118")
   end
 
-  def test_it_can_decrypt_one_character
+  def test_it_can_decrypt_one_character_and_returns_a_hash
     assert_equal ({:decryption=>"r", :key=>"12345", :offset=>"9924"}), @e.decrypt('l',"12345","041118")
   end
 
-  def test_it_can_encrypt_one_message_passing_arguments
+  def test_it_can_encrypt_one_message_passing_arguments_and_returns_a_hash
     assert_equal ({:encryption=>"bjugieejlqm", :key=>"12345", :offset=>"9924"}), @e.encrypt('hello world',"12345","041118")
   end
 
-  def test_it_can_decrypt_one_message_passing_arguments
+  def test_it_can_decrypt_one_message_passing_arguments_and_returns_a_hash
     assert_equal ({:decryption=>"hello world", :key=>"12345", :offset=>"9924"}), @e.decrypt('bjugieejlqm',"12345","041118")
   end
 
